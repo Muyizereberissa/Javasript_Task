1b4293a497db70a641b1842909eb176bc2c62270
Muyizereberissa <muyizereberissa22@gmail.com>
2024-02-05 21:41:02 +0200

    javascript tasks

--- task.js
+++ task.js
@@ -6,14 +6,13 @@ result = colors.copyWithin(0,1,3)
 console.log(result)*/
 /*const colors = ['red','green','blue','yellow','orange']
 result = colors.find()*/
-const colors3 = ['red','green','blue','yellow','orange']
-result = colors3.slice(selectedColors)
-console.log(result)
-/*const colors = ['red','green','blue','yellow','orange']
+const colors = ['red','green','blue','yellow','orange']
 const upper = colors.map(element => {
     return element.toUpperCase()
 })
-console.log(upper)*/
+console.log(upper)
+
+
 
 
 
