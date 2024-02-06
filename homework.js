1fb9084a141f25b72b377c27108ea12fdedb2287
Muyizereberissa <muyizereberissa22@gmail.com>
2024-02-06 08:23:22 +0200

    task

--- homework.js
+++ homework.js
@@ -1,5 +1,4 @@
 
-console.log(upper)
 const items = [
     {name: 'Bike', price:100},
     {name: 'TV', price:200},
@@ -10,13 +9,16 @@ const items = [
 ];
 const cheap = items.filter(item => item.price < 10)
 console.log(cheap)
+
 const expensive = items.filter(item => item.price >= 1000)
 console.log(expensive)
+
 const fullprice = items.reduce((totalPrice, currentItem) => totalPrice+currentItem)
 console.log(fullprice)
 
 const expensives = items.filter(item => item.price >= 10)
 console.log(expensive)
+
 const character = item => item.name.charAt(0).UpperCase === 'B'
 const item = items.filter(character)
 console.log(item)
